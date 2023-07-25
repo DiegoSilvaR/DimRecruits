43ee868eb23dee32f779b83a238a701b15cbc527
DiegoSilvaR <diegosilvarodriguez@hotmail.com>
2023-07-25 06:08:35 -0400

    subiendo proyecto2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+admin = User.create(email: 'diegoalejandrosilvarodriguez@gmail.com', name: 'Diego Silva', professional_title:'Diseñador', salary_expectation: '500000', skills: 'todas', password: '1985bufalo', role: :admin)
+admin = User.create(email: 'estebanrails@gmail.com', name: 'Esteban Rails', professional_title:'Administrador', salary_expectation: '500000', skills: 'todas', password: 'rails123456', role: :admin)
+
