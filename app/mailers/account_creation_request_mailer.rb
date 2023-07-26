f2826f144503d75fbbdc9e7309c8e774911e0371
Diego Silva Rodríguez <129236136+DiegoSilvaR@users.noreply.github.com>
2023-07-26 15:20:16 -0400

    Update account_creation_request_mailer.rb

--- app/mailers/account_creation_request_mailer.rb
+++ app/mailers/account_creation_request_mailer.rb
@@ -1,12 +1,12 @@
-class AccountCreationRequestMailer < ApplicationMailer
-  include Rails.application.routes.url_helpers
+# app/mailers/account_creation_request_mailer.rb
 
+class AccountCreationRequestMailer < ApplicationMailer
   default from: 'no-reply@example.com'
   layout 'mailer'
 
-  def notify_esteban
-    @request = params[:request]
-    @url = new_candidate_url # Genera el URL para la acción 'new' en el controlador 'candidates'
+  def notify_esteban(request)
+    @request = request
+    @url = 'https://dimrecruits.onrender.com/candidates/new'
     mail(to: 'estebanrails@gmail.com', subject: 'Nueva Solicitud de Creación de Cuenta')
   end
 end
