b59bb5b9a09131414dd55c23127bfff302a1c995
DiegoSilvaR <diegosilvarodriguez@hotmail.com>
2023-07-25 22:52:04 -0400

    subiendo proyecto1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :amazon
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
@@ -63,7 +63,21 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "DimRecruit_production"
 
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_caching = false
+  config.action_mailer.default_options = {from: 'no-reply@testemail.com'}
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'example.com',
+    user_name: Rails.application.credentials.gmail[ :user_name],
+    password: Rails.application.credentials.gmail[ :password],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
