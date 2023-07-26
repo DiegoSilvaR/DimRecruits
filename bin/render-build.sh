988feb931f30b730f82127f2d7846de9c5e1cdc9
DiegoSilvaR <diegosilvarodriguez@hotmail.com>
2023-07-25 23:25:26 -0400

    subiendo proyecto24

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,8 +3,8 @@
 set -o errexit
 
 bundle install
-bundle exec rake assets:precompile
+bundle exec rails webpacker:compile
 bundle exec rake assets:clean
 
-bundle exec rake db:migrate
+bundle exec rails db:migrate
 bundle exec rails db:seed
