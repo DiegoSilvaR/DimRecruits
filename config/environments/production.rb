02b2ae6de0719e4eeb119221ffb10ea451d6b7dd
DiegoSilvaR <diegosilvarodriguez@hotmail.com>
2023-07-26 23:27:48 -0400

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    domain: 'dimrecruits.onrender.com'
+    domain: 'dimrecruits.onrender.com',
     user_name: Rails.application.credentials.gmail[:user_name],
     password: Rails.application.credentials.gmail[:password],
     authentication: 'plain',
